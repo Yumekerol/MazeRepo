193d1c7d0b7207a31787a3c6e5aa6071c9467eab
Miguel Liquito <118321745+Kypoi@users.noreply.github.com>
2024-04-12 21:08:10 +0100

    Implmented The ai navigation to the enemy

---
+++ Assets/EnemyNavigation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _desiredDestination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().destination = _desiredDestination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
