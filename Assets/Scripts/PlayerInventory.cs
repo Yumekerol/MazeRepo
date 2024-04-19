45bd746b77430f3f575221a373d772eb7b4732ff
Miguel Liquito <118321745+Kypoi@users.noreply.github.com>
2024-04-19 21:27:24 +0100

    Enemy//diamond volume

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -16,7 +16,7 @@ public class PlayerInventory : MonoBehaviour
         NumberOfDiamonds++;
         OnDiamondCollected.Invoke(this);
 
-        if(NumberOfDiamonds == 2)
+        if(NumberOfDiamonds == 15)
         {
             Congrats();
         }
